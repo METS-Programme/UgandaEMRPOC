5da8bd610371761892bc11b9195056d525f2fe67
Lubwama Samuel <lubwamasamuel3@gmail.com>
2019-09-13 01:52:03 +0300

    Delete unused logger

--- omod/src/main/java/org/openmrs/module/ugandaemrpoc/fragment/controller/PrintResultsFragmentController.java
+++ omod/src/main/java/org/openmrs/module/ugandaemrpoc/fragment/controller/PrintResultsFragmentController.java
@@ -21,7 +21,6 @@ import java.util.Set;
  * Created by Francis on 2/3/2016.
  */
 public class PrintResultsFragmentController {
-	
 	public void controller(UiSessionContext sessionContext, FragmentModel model) {
 		
 		sessionContext.requireAuthentication();
